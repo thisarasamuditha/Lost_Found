ecb47d31092e21ccd3d41cc20fd68c53c302e05b
ASUS <thellamburavithanagethisarasam@gmail.com>
2025-08-05 00:35:50 +0530

    created CustomUserDetailsService class

---
+++ src/main/java/com/thisara/LNF/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.thisara.LNF.service;
+
+import com.thisara.LNF.entity.User;
+import com.thisara.LNF.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword()) // this must be hashed!
+                .roles("USER")
+                .build();
+    }
+}
