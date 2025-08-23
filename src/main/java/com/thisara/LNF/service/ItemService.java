f954cdc52f60e856325f2fb518e7124cf61545d0
ASUS <thellamburavithanagethisarasam@gmail.com>
2025-08-23 09:39:31 +0530

    updated ItemService

--- src/main/java/com/thisara/LNF/service/ItemService.java
+++ src/main/java/com/thisara/LNF/service/ItemService.java
@@ -2,10 +2,13 @@ package com.thisara.LNF.service;
 
 import com.thisara.LNF.dto.ItemRequest;
 import com.thisara.LNF.dto.ItemResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 public interface ItemService {
-    ItemResponse addItem(ItemRequest request);
+    ItemResponse createItem(ItemRequest request, MultipartFile imageFile) throws IOException;
     List<ItemResponse> getAllItems();
     ItemResponse getItemById(Long id);
     List<ItemResponse> getItemByUserId(Long userid);
